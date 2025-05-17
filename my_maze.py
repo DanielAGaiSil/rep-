a88a7f2c842904eb07a5f3969934a866aa999217
Daniel Angelus Gai Silva <danielangelusgai19@gmail.com>
2025-05-16 22:58:30 -0300

    Finished the maze AI
    Just completed the first AI shown in CS50 AI's course!

--- my_maze.py
+++ my_maze.py
@@ -5,21 +5,20 @@ class Node():
         self.state = state
         self.parent = parent
         self.action = action
-
-
+#A stack(FiLo first in last out)
 class StackFrontier():
     def __init__(self):
         self.frontier = []
-
+    
     def add(self, node):
         self.frontier.append(node)
 
     def contains_state(self, state):
         return any(node.state == state for node in self.frontier)
-
+    
     def empty(self):
         return len(self.frontier) == 0
-
+    
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
@@ -28,9 +27,8 @@ class StackFrontier():
             self.frontier = self.frontier[:-1]
             return node
 
-
+#Still a frontier, but instead of behaving like a stack, it's a queue (FiFo first in first out)
 class QueueFrontier(StackFrontier):
-
     def remove(self):
         if self.empty():
             raise Exception("empty frontier")
@@ -38,27 +36,23 @@ class QueueFrontier(StackFrontier):
             node = self.frontier[0]
             self.frontier = self.frontier[1:]
             return node
-
+        
 class Maze():
-
     def __init__(self, filename):
-
-        # Read file and set height and width of maze
+        #Read the file and set height and width of the maze
         with open(filename) as f:
-            contents = f.read()
+            contents = f.read
 
-        # Validate start and goal
+        #verifying goal and beginning
         if contents.count("A") != 1:
             raise Exception("maze must have exactly one start point")
         if contents.count("B") != 1:
             raise Exception("maze must have exactly one goal")
-
-        # Determine height and width of maze
+        
         contents = contents.splitlines()
         self.height = len(contents)
         self.width = max(len(line) for line in contents)
 
-        # Keep track of walls
         self.walls = []
         for i in range(self.height):
             row = []
@@ -67,19 +61,15 @@ class Maze():
                     if contents[i][j] == "A":
                         self.start = (i, j)
                         row.append(False)
-                    elif contents[i][j] == "B":
-                        self.goal = (i, j)
-                        row.append(False)
-                    elif contents[i][j] == " ":
+                    elif contents [i][j] == "B":
+                        self.goal(i, j)
                         row.append(False)
                     else:
                         row.append(True)
                 except IndexError:
                     row.append(False)
             self.walls.append(row)
-
         self.solution = None
-
 
     def print(self):
         solution = self.solution[1] if self.solution is not None else None
@@ -181,27 +171,22 @@ class Maze():
         for i, row in enumerate(self.walls):
             for j, col in enumerate(row):
 
-                # Walls
                 if col:
                     fill = (40, 40, 40)
 
-                # Start
                 elif (i, j) == self.start:
                     fill = (255, 0, 0)
 
-                # Goal
                 elif (i, j) == self.goal:
                     fill = (0, 171, 28)
 
-                # Solution
+                
                 elif solution is not None and show_solution and (i, j) in solution:
                     fill = (220, 235, 113)
 
-                # Explored
                 elif solution is not None and show_explored and (i, j) in self.explored:
                     fill = (212, 97, 85)
 
-                # Empty cell
                 else:
                     fill = (237, 240, 252)
 
