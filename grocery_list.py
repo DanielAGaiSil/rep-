e522ee1698798c672a4065ff6fb1a791a080b8d1
Daniel <danielangelusgai19@gmail.com>
2025-06-02 18:02:37 -0300

    Update grocery_list.py
    Just a few fixes, still not done

--- grocery_list.py
+++ grocery_list.py
@@ -1,15 +1,35 @@
-grocery_list = ["apple"]
-
+grocery_list = []
 
 def add_to_list():
-    product = input("What would you like to add to the grocery list?")
+    product = input("What would you like to add to the grocery list? ").lower
+    if type(product) != str:
+        print("Unexpected input, try again.")
     grocery_list.append(product)
 
-def remove_list():
-    grocery_list.remove()
+def remove_from_list():
+    grocery_list.remove
 
 def list_search():
-    for _ in grocery_list:
-        print(f"These are the contents: {_}")
+    for item in grocery_list:
+        print(f"These are the contents: {item}")
 
-list_search()
+def main():
+    while True:
+        print("1. Add an item to the list")
+        print("2. Remove an item from the list")
+        print("3. Show all the list's items")
+        print("4. Leave.")
+        option = input("What would you like to do to the list? ")
+        if option == "1":
+            add_to_list()
+        elif option == "2":
+            remove_from_list()
+        elif option == "3":
+            list_search()
+        elif option == "4":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid option. Try again.")
+
+main()
